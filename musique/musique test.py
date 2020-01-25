ef5697f7972dc06173cebce41ee6d1b9ee791df3
Paul Niggli <58045354+Rommel25@users.noreply.github.com>
2020-01-25 23:52:01 +0100

    Create musique test.py

---
+++ musique/musique test.py
@@ -0,0 +1,15 @@
+import pygame
+from tkinter import *
+root = Tk()
+pygame.init()
+def play():
+    pygame.mixer.music.load("musique1.mp3") #Loading File Into Mixer
+    pygame.mixer.music.play() #Playing It In The Whole Device
+Button(root,text="Play",command=play).pack()
+root.mainloop()
+#voir pour mettre la musique dès l'ouverture du jeu
+#Quand la musique ce termine son état est 0
+#if musique == 1:
+    #pygame.mixer.music.play()
+#if musique == 0:
+    #musique = 1
