9bd86f71602655852997de416cb74313085cbb5a
Tigre2325 <60426222+Tigre2325@users.noreply.github.com>
2020-02-05 11:50:19 +0100

    Ajout de la fonction création_objet

--- Repertoire_fonctions.py
+++ Repertoire_fonctions.py
@@ -2,6 +2,16 @@
 Fichier qui rencense toute les fonctions utilisées dans les différents fichier python du code
 Voire le fichier excel avec la liste des fonctions et variables pour avoir leur descritption
 """
+def creation_objet(type_objet, objet_a_creer):
+    global win
+    if type_objet == "texte":
+        objet = tkinter.Label(win, text = objet_a_creer,
+                                          font = ("Comic Sans MS", 16))
+
+    elif type_objet == "image":
+            objet = tkinter.Label(win, image = objet_a_creer)
+    
+    return objet
 
 def placement(objet, ligne, colonne, fusion_ligne = 1, fusion_colonne = 1):
     espace = 7
