16ba1f010b6bfd4cda584681f75a2cbebbfe784e
Tigre2325 <60426222+Tigre2325@users.noreply.github.com>
2020-02-22 15:33:49 +0100

    MAJ des fonctions avec le Canvas

--- Repertoire_fonctions.py
+++ Repertoire_fonctions.py
@@ -1,35 +1,45 @@
+import tkinter as tk
+
 """
 Fichier qui rencense toute les fonctions utilisées dans les différents fichier python du code
 Voire le fichier excel avec la liste des fonctions et variables pour avoir leur descritption
 """
-def creation_objet(main_frame, type_objet, objet_a_creer = " "):
-    global l_jeu
-    global h_jeu
+def creation_fenetre_graphique(titre_fenetre, taille):
+    win = tk.Tk()
+    win.title(titre_fenetre)
+    win.geometry(taille) #Taille de la fenêtre sous la forme "LargeurxHauteur"
+    
+    return win
+
+def import_image(nom_image):
+    place = "Images/" + nom_image #NE PAS OUBLIER D'AJOUTER L'EXTENSION AVEC LE NOM DE L'IMAGE!!!
+    image = tk.PhotoImage(file = place)
+    
+    return image
+
+def creation_objet(main_frame, type_objet, objet, x, y, taille = 18,
+                   couleur = "#000000"):
+    
     if type_objet == "texte":
-        objet = tkinter.Label(main_frame, text = objet_a_creer,
-                                          font = ("Comic Sans MS", 16))
-
+        objet_cree = main_frame.create_text(x, y, text = objet,
+                              font = ("Minecraft", taille), fill = couleur)
+        
     elif type_objet == "image":
-        objet = tkinter.Label(main_frame, image = objet_a_creer)
-
-        return objet
+        objet_cree = main_frame.create_image(x, y, image = objet)
+        
+    return objet_cree
     
-def creation_frame(main_frame, largeur, hauteur):
-    objet = tkinter.Frame(main_frame, width = largeur, height = hauteur)
-    
-    return objet
-
-
-def placement(objet, ligne, colonne, fusion_ligne = 1, fusion_colonne = 1):
-    espace = 7
-    objet.grid(row = ligne, column = colonne,
-                rowspan = fusion_ligne, columnspan = fusion_colonne,
-                padx = espace, pady = espace)
-
-def creation_plateau():
-    global ligne_colonne
-    pas=0
-    inc=int(700/ligne_colonne)-1
-    for pas in range(2,700,inc):
-        plateau3.create_line(pas,2,pas,700)
-        plateau3.create_line(2,pas,700,pas)
+def creation_plateau(canvas):
+    global nb_joueur #Nombre de joueur qui jouent
+    l_c = 2*nb_joueur #Nombre de lignes et de colonnes sur le plateau
+    l_offset = 286+36 #Largeur à partir du bord de la fenêtre
+    h_offset = 42 #Hauteur à partir du bord de la fenêtre
+    for i in range(1,l_c):
+        #Création des lignes
+        canvas.create_line(l_offset, h_offset+632-(632/l_c)*i,
+                           l_offset+636, h_offset+632-(632/l_c)*i,
+                           width = 10-(nb_joueur-2)*2, fill = "#7A4330") #Couleur pour le plateau_1
+        #Création des colonnes
+        canvas.create_line(l_offset+632-(632/l_c)*i, h_offset,
+                           l_offset+632-(632/l_c)*i, h_offset+636,
+                           width = 10-(nb_joueur-2)*2, fill = "#7A4330")
