12a281be9f4ce7859bdee38f439dfa87ed6b2e75
Tigre2325 <60426222+Tigre2325@users.noreply.github.com>
2020-02-22 15:27:16 +0100

    Fenêtre graphique en utilisant le Canvas

--- Anthony/Fonctions_Anthony.py
+++ Anthony/Fonctions_Anthony.py
@@ -1,90 +1,100 @@
-import tkinter
-
+import tkinter as tk
+  
 #Variables
 l_jeu = 1280
 h_jeu = 720
 res_jeu = str(l_jeu) + "x" + str(h_jeu)
 nom_fenetre = "SYNAPSE 2 Joueurs"
-cote = 700
-espace = 2
+nb_joueur = 2
+
+pad1 = 10
+pad2 = 20
+pad3 = 40
+c_img = 55
+padH = h_jeu*7/10-pad2-2*pad1-2.5*c_img
+padH2 = padH-6*pad2
+padB = h_jeu*7/10-pad2-0.5*c_img
+padD = l_jeu-pad3-0.5*c_img
+padG = l_jeu-pad3-pad2-2.5*c_img
+
 
 #Fonctions
-def import_image(nom_image):
-    place = "images/" + nom_image + ".gif"
-    image = tkinter.PhotoImage(file = place)
-    return image
-
-def creation_objet(main_frame, type_objet, objet_a_creer = " "):
-    if type_objet == "texte":
-        objet = tkinter.Label(main_frame, text = objet_a_creer,
-                                          font = ("Comic Sans MS", 16))
-
-    elif type_objet == "image":
-        objet = tkinter.Label(main_frame, image = objet_a_creer)
-
-    return objet
-
-def creation_frame(main_frame, largeur, hauteur, couleur = ""):
-    objet = tkinter.Frame(main_frame, width = largeur, height = hauteur,
-                          bg = couleur)
-
-    return objet
-
-
-def placement(objet, ligne, colonne, fusion_ligne = 1, fusion_colonne = 1):
-    global espace
-    objet.grid(row = ligne, column = colonne,
-                rowspan = fusion_ligne, columnspan = fusion_colonne,
-                padx = espace, pady = espace)
-
 def creation_fenetre_graphique(titre_fenetre, taille):
-    win = tkinter.Tk()
+    win = tk.Tk()
     win.title(titre_fenetre)
     win.geometry(taille)
-    
     return win
 
+def import_image(nom_image):
+    place = "Images/" + nom_image
+    image = tk.PhotoImage(file = place)
+    return image
+
+
+def creation_objet(main_frame, type_objet, objet, x, y, taille = 18,
+                   couleur = "#000000"):
+    if type_objet == "texte":
+        objet_cree = main_frame.create_text(x, y, text = objet,
+                              font = ("Minecraft", taille), fill = couleur)
+    elif type_objet == "image":
+        objet_cree = main_frame.create_image(x, y, image = objet)
+    return objet_cree
+
+
+
+
+def creation_plateau(canvas):
+    global nb_joueur #Nombre de joueur qui jouent
+    l_c = 2*nb_joueur #Nombre de lignes et de colonnes sur le plateau
+    l_offset = 286+36 #Largeur à partir du bord de la fenêtre
+    h_offset = 42 #Hauteur à partir du bord de la fenêtre
+    for i in range(1,l_c):
+        #Création des lignes
+        canvas.create_line(l_offset, h_offset+632-(632/l_c)*i,
+                           l_offset+636, h_offset+632-(632/l_c)*i,
+                           width = 10-(nb_joueur-2)*2, fill = "#7A4330")
+        #Création des colonnes
+        canvas.create_line(l_offset+632-(632/l_c)*i, h_offset,
+                           l_offset+632-(632/l_c)*i, h_offset+636,
+                           width = 10-(nb_joueur-2)*2, fill = "#7A4330")
+        
+
+
+    
+
 #Fenetre graphique
-win2 = creation_fenetre_graphique(nom_fenetre, res_jeu)
+win = creation_fenetre_graphique(nom_fenetre, res_jeu)
+c = tk.Canvas(win, width = l_jeu, height = h_jeu)
+c.pack() #Placement du canvas au milieu de la fenêtre
+
+#Importation des images
+img_fond = import_image("Bois_brut_tres_clair.png") #Background du jeu
+img_plateau = import_image("Plateau_1.png")
+img_FH = import_image("Fleche_haut_1.png")
+img_FB = import_image("Fleche_bas_1.png")
+img_FD = import_image("Fleche_droite_1.png")
+img_FG = import_image("Fleche_gauche_1.png")
+img_chiffre = import_image("Chiffre_1.png") #Fond des chiffres du tableau de bord
 
 
-plateau_frame = creation_frame(win2, h_jeu-2*espace, h_jeu-2*espace)
-placement(plateau_frame, 0, 1, 10, 1)
+#Placement des images
+fond = creation_objet(c, "image", img_fond, l_jeu/2, h_jeu/2)
+plateau = creation_objet(c, "image", img_plateau, l_jeu/2, h_jeu/2)
+creation_plateau(c)
 
-score_P1_frame = creation_frame(win2, (l_jeu-h_jeu)/2-2*espace, h_jeu/2-2*espace, "green")
-placement(score_P1_frame, 0, 0, 5, 1)
+chiffre_1 = creation_objet(c, "image", img_chiffre, padG, padH2)
+chiffre_2 = creation_objet(c, "image", img_chiffre, (padG+padD)/2, padH2)
+chiffre_3 = creation_objet(c, "image", img_chiffre, padD, padH2)
 
-score_P2_frame = creation_frame(win2, (l_jeu-h_jeu)/2-2*espace, (h_jeu/2)-2*espace, "blue")
-placement(score_P2_frame, 5, 0, 5, 1)
+FH = creation_objet(c, "image", img_FH, (padD+padG)/2, padH)
+FB = creation_objet(c, "image", img_FB, (padD+padG)/2, padB)
+FD = creation_objet(c, "image", img_FD, padD, (padH+padB)/2)
+FG = creation_objet(c, "image", img_FG, padG, (padH+padB)/2)
+
+#Chiffres
+C1 = creation_objet(c, "texte", "1", padG, padH2, 22, "#CEAF91")
+C2 = creation_objet(c, "texte", "2", (padG+padD)/2, padH2, 22, "#CEAF91")
+C3 = creation_objet(c, "texte", "3", padD, padH2, 22, "#CEAF91")
 
 
-commande_frame = creation_frame(win2, (l_jeu-h_jeu)/2-2*espace, (7*h_jeu/10)-2*espace, "green")
-placement(commande_frame, 0, 2, 7, 1)
-
-piece_frame = creation_frame(win2, (l_jeu-h_jeu)/2-2*espace, (3*h_jeu/10)-2*espace, "blue")
-placement(piece_frame, 7, 2, 3, 1)
-
-"""
-plateau2 = creation_objet(plateau_frame, "image", import_image("plateau"))
-placement(rien, 0,0)
-
-plateau2 = tkinter.Canvas(plateau_frame, width = cote, height = cote)
-placement(plateau2, 0, 0) 
-
-#Colonnes du plateau
-plateau2.create_line(2, 2, 2, cote)
-plateau2.create_line((cote/4), 0, (cote/4), cote)
-plateau2.create_line((cote/2), 0, (cote/2), cote)
-plateau2.create_line((cote*3/4), 0, (cote*3/4), cote)
-plateau2.create_line(cote, 0, cote, cote)
-
-#Lignes du plateau
-plateau2.create_line(2, 2, cote, 2)
-plateau2.create_line(0, (cote/4), cote, (cote/4))
-plateau2.create_line(0, (cote/2), cote, (cote/2))
-plateau2.create_line(0, (cote*3/4), cote, (cote*3/4))
-plateau2.create_line(0, cote, cote, cote)
-"""
-
-win2.mainloop()
-
+win.mainloop()
